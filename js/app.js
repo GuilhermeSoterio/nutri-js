8fd8c385d18ca9522f00e5bb4392f79127da8c22
Guilherme Sotério <guilhermehenrique.oliv3@gmail.com>
2020-11-29 15:13:09 -0300

    Logical operators to make the necessary checks of the IMC

--- js/app.js
+++ js/app.js
@@ -1,4 +1,35 @@
 var titulo = document.querySelector("h1");
 			//por classe
-			var titulo2 = document.querySelector(".titulo");
-			titulo.textContent = "Banana";
+var titulo2 = document.querySelector(".titulo");
+titulo.textContent = "Banana";
+//---------------------Selecionando Elementos----------------------
+var paciente = document.querySelector("#primeiro-paciente");
+
+var tdPeso = paciente.querySelector(".info-peso");
+var peso = tdPeso.textContent;
+
+var tdAltura = paciente.querySelector(".info-altura");
+var altura = tdAltura.textContent;
+
+var tdImc = paciente.querySelector(".info-imc");
+
+//---------------------
+var pesoEhValido = true;
+var alturaEhValida = true;
+
+if(peso <= 0 || peso >= 1000) {
+    console.log("peso inválido!");
+    pesoEhValido = false;
+    tdImc.textContent = "peso Inválido";
+}
+
+if(altura <= 0 || altura >= 3.00) {
+    console.log("altura inválido!");
+    alturaEhValida = false;
+    tdImc.textContent = "Altura Inválido";
+}
+//--------------------------Calculando IMC ------------------------
+if( alturaEhValida && pesoEhValido){
+    var imc = peso / (altura * altura);
+    tdImc.textContent = imc;
+}
